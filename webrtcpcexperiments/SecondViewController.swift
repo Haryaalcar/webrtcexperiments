a20998fb02a2d6e1c2a2a28857c1b91b9d81497f
Haryaalcar <landby@gmail.com>
2019-02-19 21:49:49 +0300

    Add project

---
+++ webrtcpcexperiments/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  webrtcpcexperiments
+//
+//  Created by Uladzislau Susha on 2/19/19.
+//  Copyright © 2019 Uladzislau Susha. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ webrtcpcexperimentsTests/webrtcpcexperimentsTests.swift
@@ -0,0 +1,34 @@
+//
+//  webrtcpcexperimentsTests.swift
+//  webrtcpcexperimentsTests
+//
+//  Created by Uladzislau Susha on 2/19/19.
+//  Copyright © 2019 Uladzislau Susha. All rights reserved.
+//
+
+import XCTest
+@testable import webrtcpcexperiments
+
+class webrtcpcexperimentsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
